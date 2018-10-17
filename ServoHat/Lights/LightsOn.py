4a9b0b7c0fce3135c190c2b27f7dd0c5ffa1a4b8
Scott S <schretzilla@gmail.com>
2018-10-17 04:29:39 +0000

    Updated labeling of ports rgb

--- ServoHat/Lights/LightsOn.py
+++ ServoHat/Lights/LightsOn.py
@@ -6,16 +6,16 @@ PWM = Adafruit_PCA9685.PCA9685(address=0x60)
 PWM.set_pwm_freq(60)
 
 #LED Pins
+GreenPin = 12
 RedPin = 13
-GreenPin = 14
-BluePin = 15
+BluePin = 14
 
 MaxPower = 4095
 
 #Turn on all leds at full power
 def LightupLeds():
         PWM.set_pwm(RedPin, 0, MaxPower)
-        PWM.set_pwm(GreenPin, 0, MaxPower)
+        #PWM.set_pwm(GreenPin, 0, MaxPower)
         PWM.set_pwm(BluePin, 0, MaxPower)    
     
 if __name__ == '__main__':
