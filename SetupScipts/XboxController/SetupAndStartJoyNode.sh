e2ddf6cf379cb8d9a75df7faacf608171f488b7d
Scott Schretzenmaier <schretzy@gmail.com>
2018-10-18 19:15:40 -0700

    Added setup and start script for Xbox controller

---
+++ SetupScipts/XboxController/SetupAndStartJoyNode.sh
@@ -0,0 +1,16 @@
+MY_IP=$1
+if [ $# -eq 0 ]
+then 
+    echo "Environment Variables not setup!"
+    echo "Provide the Masters IP address as arg 1"
+else
+    export ROS_IP=$MY_IP
+    echo "ROS_IP=$ROS_IP"
+    export ROS_MASTER_URI="http://$MY_IP:11311"
+    echo "ROS_MASTER_URI=$ROS_MASTER_URI"
+    rosparam set joy_node/dev "/dev/input/js1"
+    echo "joy node attached to /dev/input/js1"
+    echo "Starting Joy Node"
+    rosrun joy joy_node
+fi
+
